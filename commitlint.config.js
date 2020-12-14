febef0355ef8d2337ed31e0677c4f977a7d98670
jonlazarini <jlazarini@deloitte.co.uk>
2020-12-14 19:39:24 +0000

    CHORE: enable commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+// https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+// https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'type-case': [2, 'always', 'upper-case'],
+    'type-enum': [2, 'always', ['REVERT', 'FEAT', 'TEST', 'FIX', 'CHORE', 'CI', 'DOCS', 'BREAKING_CHANGE', 'REFACTOR']],
+    'subject-case': [2, 'always', 'lower-case'],
+    // level: disabled to use our own rule with commitlint-plugin-function-rules plugin
+    'scope-case': [0],
+    'function-rules/scope-empty': [
+      1,
+      'always',
+      (parsed) => {
+        // type(scope?): subject
+        if (parsed?.scope && parsed.scope.match(/^CVSB-\d+$/) !== null) {
+          return [true];
+        } else {
+          return [false, 'scope must have the following format: (CVSB-XXXX) where XXXX is a number'];
+        }
+      },
+    ],
+  },
+};
