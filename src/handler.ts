3f5ae44aed3e352239433f8222ce605ad53f2316
jonlazarini <jlazarini@deloitte.co.uk>
2020-12-15 16:09:50 +0000

    TEST: add unit test, refactor handler and dev scripts

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,9 @@
-import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 import 'source-map-support/register';
 
 console.log(`\nrunning version: ${process.env.API_VERSION} of API\n\n`);
 
-export async function hello(
-  event: APIGatewayEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> {
-
+export async function hello(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
   console.log(`with context in Handler: ${JSON.stringify(context, null, 2)}`);
 
   return {
@@ -21,4 +17,4 @@ export async function hello(
       2,
     ),
   };
-};
+}
