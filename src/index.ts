a079bbdf59f262d13fb7109ebb4a34f9c8a0f782
Tanio Artino <tartino@.deloitte.co.uk>
2021-02-09 14:59:10 +0000

    refactored code to allow new versioning
    Signed-off-by: Tanio Artino <tartino@.deloitte.co.uk>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import 'source-map-support/register';
-import serverless from 'serverless-http';
-import { Context, APIGatewayEvent, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = async (event = {}) => {
+    console.log('Hello trailer v1.0.2!');
+    const response = JSON.stringify(event, null, 2);
+    return response;
+};
 
-import { app } from './infrastructure/api';
-
-const { NODE_ENV, SERVICE_VERSION, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE } = process.env;
-
-console.log(
-  `\nRunning Service\n version: '${SERVICE_VERSION}'\n mode: ${NODE_ENV}\n stage: '${AWS_PROVIDER_STAGE}'\n region: '${AWS_PROVIDER_REGION}'\n\n`,
-);
-
-const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyStructuredResultV2> =>
-  await serverless(app)(event, context);
-
-export { handler };
