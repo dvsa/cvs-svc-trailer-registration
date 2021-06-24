699c6fce625d9a4a5f837a3bb24fb8a5a7c7e40f
Umer Jaffer <ujaffer@deloitte.co.uk>
2021-06-24 11:36:06 +0100

    chore(cvsb-18921): remove comments, unused variables and files.

--- src/handler.ts
+++ src/handler.ts
@@ -5,14 +5,6 @@ import { Context, APIGatewayEvent, APIGatewayProxyStructuredResultV2 } from 'aws
 import { app } from './infrastructure/api';
 import { debug } from './utils/logger';
 
-// const {
-//   NODE_ENV, SERVICE, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE,
-// } = process.env;
-
-// info(
-//   `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_PROVIDER_STAGE}'\n region: '${AWS_PROVIDER_REGION}'\n\n`,
-// );
-
 const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
   debug('event');
   debug(JSON.stringify(event.pathParameters, null, 2));
