bf3a14f326babdcef7636c619f2acc1ec3d2ad78
Guillaume Julien <guillaume.julien@bjss.com>
2023-07-26 12:45:34 +0100

    somewhat a fix

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,49 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line import/no-import-module-exports
 import { spawn } from 'child_process';
+// process.on('error', (err: string) => {
+//   console.log('\nSomething wrong happened :(\n\n');
+//   console.error(`err: ${err}`);
+//   reject(err);
+// });
 
 // We hook to serverless offline when firing its process
-const SERVER_OK = 'server ready: http://localhost:3020';
+const SERVER_OK = 'localhost:3020';
 // Serverless fires a local dynamo-db instance which is killed once the parent process is terminated
 // the current serverless script checks whether a local instance is running but does not error when binding fails
 // we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
 const DYNAMO_LOCAL_ERROR_THREAD = 'Exception in thread "main"';
 
-const setupServer = (process: NodeJS.Process): Promise<NodeJS.Process> => new Promise((resolve, reject) => {
-  process.stdout.setEncoding('utf-8').on('data', (stream: [string]) => {
-    // last message emitted by webpack once the server is up and running
+const setupServer = () => new Promise<void>((resolve, reject) => {
+  const server = spawn('npm', ['run', 'start']);
+  server.stdout.setEncoding('utf-8').on('data', (stream: string[]) => {
     if (stream.includes(SERVER_OK)) {
-      resolve(process);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      resolve();
     }
   });
 
-  process.stderr.setEncoding('utf-8').on('data', (stream: [string]) => {
+  server.stderr.setEncoding('utf-8').on('data', (stream: string[]) => {
+    console.log(stream);
     if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
+      reject();
       throw new Error('Internal Java process crashed');
-    }
-    reject(stream);
-  });
-
-  process.on('error', (err: string) => {
-    console.log('\nSomething wrong happened :(\n\n');
-    console.error(`err: ${err}`);
-    reject(err);
-  });
-
-  process.on('exit', (code: number, signal: string) => {
-    if (code !== 137) {
-      console.info(`process terminated with code: ${code} and signal: ${signal}`);
     }
   });
 });
 
-const server = spawn('npm', ['run', 'start']);
-
 module.exports = async (): Promise<void> => {
   console.log('\nSetting up Integration tests...\n\n');
   try {
-    await setupServer(server as any);
+    await setupServer();
   } catch (e) {
-    console.error('Something wrong happened:\n');
+    console.error('Something wrong happened: 2\n');
     console.error(e);
     process.exit(1);
   }
